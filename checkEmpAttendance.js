956c84968b3c45dccbb9257b3e5c0c2d4d314996
Darpan Madlani <Darpan@proclink.com>
2022-01-20 20:48:36 +0530

    switch case implemented

--- checkEmpAttendance.js
+++ checkEmpAttendance.js
@@ -7,15 +7,21 @@ let isPresent = Math.floor(Math.random() * 3);
 
 let dailyWage;
 
-if (isPresent == PRESENT) {
-    console.log("Employee is present");
-    dailyWage = WORK_HOUR * PAY_PER_HOUR
-} else if (isPresent == PART_TIME) {
-    console.log("Employee is working part time");
-    dailyWage = (WORK_HOUR / 2) * PAY_PER_HOUR;
-} else {
-    console.log("Employee is absent");
-    dailyWage = 0;
+switch (isPresent) {
+    case PRESENT:
+        console.log("Employee is present");
+        dailyWage = WORK_HOUR * PAY_PER_HOUR;
+        break;
+
+    case PART_TIME:
+        console.log("Employee is working part time");
+        dailyWage = (WORK_HOUR / 2) * PAY_PER_HOUR;
+        break;
+
+    default:
+        console.log("Employee is absent");
+        dailyWage = 0;
+        break;
 }
 
 console.log("Daily wage : " + dailyWage + " USD");
---
+++ functionDemo.js
@@ -0,0 +1,7 @@
+function addNumber(a,b)  {
+    return a+b;
+}
+
+let x= addNumber(true,"5");
+
+console.log(x);
